68dde189716f381d2c1c2120c7b3da0e83311f4c
ErickMoxley <exilerzero@yahoo.com>
2019-12-01 16:44:02 -0500

    update

--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ import initTilt from './js/tilt';
 import initSr from './js/sr';
 import './style/main.scss';
 
-$('a[href^="#"]').on('click', function(event) {
+$('a[href^="#"]').on('click', function (event) {
   var target = $(this.getAttribute('href'));
   if (target.length) {
     event.preventDefault();
     $('html, body')
       .stop()
-      .animate(
-        {
+      .animate({
           scrollTop: target.offset().top
         },
         1000
